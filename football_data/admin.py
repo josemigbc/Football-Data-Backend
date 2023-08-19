6aa6f6bb5eb25b1f9396e0520e756aaeff5d8d49
Jose_Miguel <apolo13x@gmail.com>
2023-08-19 11:57:13 -0400

    Created the models for Football Data

--- football_data/admin.py
+++ football_data/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import (
+    Competition,Scorers,Standings,Match
+)
 # Register your models here.
+
+admin.site.register(Competition)
+admin.site.register(Match)
+admin.site.register(Standings)
+admin.site.register(Scorers)
--- football_data/models.py
+++ football_data/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+# Create your models here
+
+class Competition(models.Model):
+    slug = models.SlugField(_("Slug"),unique=True)
+    data = models.JSONField(_("Data"))
+
+    class Meta:
+        verbose_name = 'Competition'
+        verbose_name_plural = 'Competitions'
+
+    def __str__(self):
+        return self.data.get('name')
+
+class Match(models.Model):
+    
+    id = models.IntegerField(_("Id"))
+    competition = models.CharField(_("Competition"), max_length=5)
+    home_team_id = models.IntegerField(_("Home Team Id"))
+    away_team_id = models.IntegerField(_("Away Team Id"))
+    datetime = models.DateTimeField(_("Date Time"))
+    data = models.JSONField(_("Data"))
+
+    class Meta:
+        verbose_name = 'Match'
+        verbose_name_plural = 'Matches'
+
+    def __str__(self):
+        return f"Match {self.id}"
+
+class Standings(models.Model):
+    
+    competition = models.CharField(_("Competition"), max_length=5)
+    data = models.JSONField(_("Data"))
+
+    class Meta:
+        verbose_name = 'Standings'
+        verbose_name_plural = 'Standings'
+
+    def __str__(self):
+        return f"Standings {self.competition}"
+
+class Scorers(models.Model):
+    
+    competition = models.CharField(_("Competition"), max_length=5)
+    data = models.JSONField(_("Data"))
+
+    class Meta:
+        verbose_name = 'Scorers'
+        verbose_name_plural = 'Scorers'
+
+    def __str__(self):
+        return f"Scorers {self.competition}"
