2f79fbab409fd8fee9c9040e06fc255116e5355e
Jose_Miguel <apolo13x@gmail.com>
2023-08-23 10:14:21 -0400

    Reduction for production

--- fivethirtyeight/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import FootballCompetition,FootballMatch,FootballTeam
-
-# Register your models here.
-
-admin.site.register(FootballTeam)
-admin.site.register(FootballCompetition)
-admin.site.register(FootballMatch)
--- fivethirtyeight/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FivethirtyeightConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fivethirtyeight'
